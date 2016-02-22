87996b0db39458c98f0f375b64339456f24e5d2f
Doracool <asas13525482040@163.com>
2016-02-22 10:58:12 +0800

    实现简单的群聊客户端

--- 环信/群聊客户端/群聊客户端/ViewController.m
+++ 环信/群聊客户端/群聊客户端/ViewController.m
@@ -8,16 +8,26 @@
 
 #import "ViewController.h"
 #import "GCDAsyncSocket.h"
-@interface ViewController ()<GCDAsyncSocketDelegate,UITableViewDataSource>
+@interface ViewController ()<GCDAsyncSocketDelegate,UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *uitextFiled;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
-@property (weak, nonatomic) IBOutlet UITableView *UITableView;
+
 - (IBAction)senderAction:(id)sender;
 @property (nonatomic, strong) GCDAsyncSocket *clientSocket;
+@property (nonatomic, strong) NSMutableArray *dataSource;
 @end
 
 @implementation ViewController
 
+
+-(NSMutableArray *)dataSource
+{
+    if (!_dataSource) {
+        _dataSource = [NSMutableArray array];
+    }
+    return _dataSource;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -57,17 +67,47 @@
     NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"%@",str);
     
+    if (str) {
+        [self.dataSource addObject:str];
+        
+        //刷新表格
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            [self.tableView reloadData];
+        }];
+    }
+    
     [sock readDataWithTimeout:-1 tag:0];
 }
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 0;
+    return 1;
 }
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataSource.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ID = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    
+    //显示文字
+    cell.textLabel.text = self.dataSource[indexPath.row];
+    return cell;
+}
+
 - (IBAction)senderAction:(id)sender {
     NSString *str = self.uitextFiled.text;
     if (str.length == 0) {
         return;
     }
+    
+    //把数据添加到数据源
+    [self.dataSource addObject:self.uitextFiled.text];
+    
+    //刷新表格
+    [self.tableView reloadData];
     
     //发数据
     [self.clientSocket writeData:[str dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1 tag:0];
